de5e6b867934bda6e3c6bb85f2033937993b39fd
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-03-22 00:21:29 +0900

    Add userInput()

--- crawler/appstore_crawler.py
+++ crawler/appstore_crawler.py
@@ -2,7 +2,34 @@
 import json, requests
 import pandas as pd
 import csv
+import sys
+
+def userInput():
+    type_list = ['movie', 'podcast', 'music', 'musicVideo', 'audiobook', 'shortFilm', 'tvShow', 'software', 'ebook', 'all']
+
+    term = input("Please enter a word to search for: ")
+    term = term.replace(" ", "+")
+
+    print("Please enter the number of the type of results you want returnd ex) 1")
+    print("1.movie 2.podcast 3.music 4.musicVideo 5.audiobook 6.shortFilm 7.tvShow 8.software(app) 9.ebook 10.all")
+    type_num = input()
+    type_str = type_list[int(type_num) - 1]
+    print(type_str)
+
+    cntry = input("Please enter the two-letter country code for the store you want to search: ")
+    cntry = cntry.lower()
+    if(len(cntry) == 0):
+        cntry = 'us'
+    elif(len(cntry) != 2):
+        print("You've entered a wrong country code!")
+        sys.exit(1)
+    
+    return [term, type_str, cntry]
+        
+
 def main():
+    term, type_str, cntry = userInput()
+
     r = requests.get("https://itunes.apple.com/search", params={
         'term': term,
         'country': cntry,
